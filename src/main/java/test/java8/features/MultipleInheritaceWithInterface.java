05052378d28be29ed1f60b8f54607a4cef370ebd
husenaiah.g <husenaiah.gandham@kagamierp.com>
2021-12-01 23:16:01 +0530

    MultipleInheritaceWithInterface

---
+++ src/main/java/test/java8/features/MultipleInheritaceWithInterface.java
@@ -0,0 +1,39 @@
+package test.java8.features;
+
+
+
+
+interface Left{
+	
+	default void m1()
+	{
+		System.out.println("interface left default method ");
+	}
+}
+
+interface Right{
+	
+	default void m1()
+	{
+		System.out.println("interface right default method ");
+	}
+}
+
+
+public class MultipleInheritaceWithInterface implements  Left,Right{
+	
+	public void m1()
+	{
+		System.out.println("own default method ");
+		Left.super.m1();
+		Right.super.m1();
+	}
+	
+	public static void main(String args[]) {
+		
+		MultipleInheritaceWithInterface m=new MultipleInheritaceWithInterface();
+		m.m1();
+		
+	}
+
+}
