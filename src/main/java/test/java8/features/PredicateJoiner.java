7863383fd2334f1e5ba2e2d1618471474c8115c1
husenaiah.g <husenaiah.gandham@kagamierp.com>
2021-12-04 15:00:04 +0530

    predicate joiner

---
+++ src/main/java/test/java8/features/PredicateJoiner.java
@@ -0,0 +1,43 @@
+package test.java8.features;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PredicateJoiner {
+
+	public static void main(String args[]) {
+
+		int[] array = { 23, 12, 5, 26, 27, 13, 14, 11 };
+
+		Predicate<Integer> p1 = i -> i % 2 == 0;
+
+		Predicate<Integer> p2 = i -> i > 10;
+		
+		// print nos which are even and grater than 10
+		for (int a : array) {
+
+			if (p1.and(p2).test(a)) {
+				System.out.println(a);
+			}
+		}
+		System.out.println("-----------");
+		// print nos which are even or grater than 10
+				for (int a : array) {
+
+					if (p1.or(p2).test(a)) {
+						System.out.println(a);
+					}
+				}
+				System.out.println("-----------");
+	      // print nos which are not even (opposite to even login)
+				
+				for (int a : array) {
+
+					if (p1.negate().test(a)) {
+						System.out.println(a);
+					}
+				}
+	}
+
+}
+
--- src/main/java/test/java8/features/PredictClass.java
+++ src/main/java/test/java8/features/PredictClass.java
@@ -1,21 +1,45 @@
 package test.java8.features;
 
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
-public class PredictClass  {
+class Employee {
 	
+	int empno;
+	String ename;
+	public Employee(int empno, String ename) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+	}
 	
+}
+
+public class PredictClass {
+
 	public static void main(String args[]) {
-	
-	    Predicate<Integer> p=i->i%2==0;
-	    
-	    System.out.println("even no test   -> "+p.test(10));
-	    System.out.println("even no test   -> "+p.test(11));
-	    System.out.println("even no test   -> "+p.test(12));
-	    
-	    
-	    
-	
+
+		Predicate<Integer> p = i -> i % 2 == 0;
+
+		System.out.println("even no test   -> " + p.test(10));
+		System.out.println("even no test   -> " + p.test(11));
+		System.out.println("even no test   -> " + p.test(12));
+
+		ArrayList<Employee> emp = new ArrayList<Employee>();
+		emp.add(new Employee(4, "ABC"));
+		emp.add(new Employee(3, "XYY"));
+		emp.add(new Employee(1, "STU"));
+		emp.add(new Employee(2, "DEF"));
+
+		Predicate<Employee> empdata = e -> e.empno > 2;
+
+		for (Employee e : emp) {
+
+			if (empdata.test(e)) {
+				System.out.println("emp name whose id > 1   -> " + e.ename + " emp id " + e.empno);
+			}
+		}
+
 	}
 
 }
